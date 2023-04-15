625710cc640ebb9c00c9db530d3bd84ac50b3fad
Mm6585 <Mm6585@gmail.com>
2023-04-15 21:56:14 +0900

    main() -> image.save_masked_vid()

--- save_masked_vid.py
+++ save_masked_vid.py
@@ -1,74 +1,8 @@
-from rtmdet import RTMDet
-import pipeline as pl
 import image
-import os
-import cv2
-import numpy as np
-import argparse
-from time import time
+import pipeline as pl
+from rtmdet import RTMDet
 
-def main(opt):
-    device = opt.device
-    ws_path = os.getcwd()
-    track_path = os.path.join(ws_path, 'results/track')
-    os.makedirs(track_path, exist_ok=True)
-    index = len(os.listdir(track_path)) + 1
-    save_path = os.path.join(track_path, 'result' + str(index))
-    os.makedirs(save_path, exist_ok=True)
-
-    model = RTMDet(device)
-    pipeline = pl.create_pipeline()
-
-    frame_size = (640, 480)
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    fps = 30.0
-    file_path = os.path.join(save_path, 'result.avi')
-    out = cv2.VideoWriter(file_path, fourcc, fps, frame_size)
-
-    elapsed_time = 0
-    i = 0
-    try:
-        while (True):
-            color_image, _ = image.get_images(pipeline)
-
-            # get detection result
-            result, pred_time = model.get_prediction(color_image, [0])
-            if (result == None):
-                print('frame', i, ': object not found for the specified classes')
-                i += 1
-                continue
-            masked_image = model.get_masked_image(color_image, result)
-
-            print('frame', i, ': prediction time =', round(pred_time, 2), 's')
-            cv2.imshow('masked image', masked_image)
-
-            # save video
-            out.write(masked_image)
-            elapsed_time += pred_time
-
-            if (cv2.waitKey(1) == 27):
-                break
-
-            i += 1
-
-    finally:
-        elapsed_time = round(elapsed_time, 2)
-        print('-----------------------------------------')
-        print('job finished')
-        print('total elapsed time =', elapsed_time)
-        print('average elapsed time per frame =', round(elapsed_time/i, 2))
-        print('-----------------------------------------')
-        pl.stop_pipeline(pipeline)
-        out.release()
-        cv2.destroyAllWindows()
-
-def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--device', type=str, default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu or etc..')
-    parser.add_argument('--threshold', type=float, default=0.5, help='threshold')
-    
-    return parser.parse_args()
-
-if __name__ == "__main__":
-    opt = parse_opt()
-    main(opt)
+device = 'cpu'
+pipeline = pl.create_pipeline()
+model = RTMDet(device)
+image.save_masked_vid(model, pipeline)
