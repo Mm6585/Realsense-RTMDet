e5732c504b5145fab3b81ce45b2f9ee7b3c0893e
Mm6585 <Mm6585@gmail.com>
2023-04-15 21:57:52 +0900

    coco dataset classes used to train the model & colors to draw masks with random colors for 80 classes

---
+++ cfg.py
@@ -0,0 +1,25 @@
+def get_classes():
+
+    classes = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 
+            'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 
+            'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 
+            'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 
+            'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 
+            'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 
+            'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 
+            'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+    
+    return classes
+
+def get_colors():
+    
+    colors = [(0, 0, 255), (154, 111, 101), (255, 0, 0), (190, 121, 13), (91, 82, 236), (254, 59, 219), (191, 152, 104), (63, 7, 65), (117, 207, 226), (223, 248, 47), 
+            (146, 52, 241), (148, 135, 234), (200, 68, 202), (231, 3, 185), (227, 92, 140), (80, 126, 167), (6, 65, 103), (40, 78, 254), (167, 191, 213), (4, 31, 173), 
+            (161, 142, 211), (58, 5, 14), (90, 239, 210), (199, 31, 123), (26, 247, 231), (103, 8, 140), (68, 104, 102), (112, 90, 84), (23, 83, 16), (88, 175, 84), 
+            (110, 81, 222), (101, 74, 157), (100, 21, 218), (53, 54, 70), (230, 39, 12), (93, 94, 20), (62, 176, 179), (166, 183, 124), (108, 151, 95), (33, 123, 6), 
+            (30, 197, 44), (183, 19, 16), (58, 84, 37), (107, 157, 185), (232, 143, 129), (134, 35, 58), (237, 92, 132), (125, 46, 115), (104, 176, 95), (14, 26, 1), 
+            (70, 245, 30), (51, 189, 181), (64, 159, 82), (165, 59, 219), (11, 163, 47), (38, 238, 148), (188, 102, 23), (45, 245, 188), (255, 201, 190), (254, 227, 162), 
+            (90, 108, 189), (122, 27, 97), (44, 230, 239), (32, 138, 108), (253, 29, 249), (128, 161, 26), (152, 23, 251), (233, 22, 147), (187, 11, 164), (39, 200, 75), 
+            (209, 72, 171), (171, 4, 169), (243, 182, 142), (7, 140, 112), (233, 13, 181), (3, 115, 187), (123, 18, 196), (55, 99, 195), (98, 109, 223), (34, 248, 147)]
+    
+    return colors
