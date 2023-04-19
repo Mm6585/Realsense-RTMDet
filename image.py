979c60e3d34d11928e80341d9b87b7c5751e168b
Mm6585 <Mm6585@gmail.com>
2023-04-19 20:26:32 +0900

    image.save_masked_vid -> save_masked_vid.py & update to masking depth image

--- image.py
+++ image.py
@@ -23,13 +23,13 @@ def get_images(pipeline, type=0):
     if (type == 1):
         return color_image
     
-    depth_image = np.asanyarray(aligned_depth_frame.get_data())
-    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+    depth_image = np.asanyarray(aligned_depth_frame.get_data(), 'uint8')
+    # depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
     if (type == 2):
         return depth_image
         # return depth_colormap
 
-    return color_image, depth_colormap
+    return color_image, depth_image #depth_colormap
 
 # def get_masked_image(model, color_image, result):
 
@@ -44,7 +44,10 @@ def get_images(pipeline, type=0):
 #     )
 #     return visualizer.get_image()
 
-def get_masked_image(color_image, result, threshold=0.5):
+def get_masked_image(image, result, threshold=0.5):
+    if (np.isscalar(image[0][0])):
+        image = np.stack((image, image, image), axis=2).squeeze()
+
     _class, mask, score, bbox = result[0], result[1], result[2], result[3]
 
     mask_list, class_list, score_list, bbox_list = [], [], [], []
@@ -61,18 +64,20 @@ def get_masked_image(color_image, result, threshold=0.5):
             p1 = (int(left), int(top))
             p2 = (int(left+w), int(top+h))
             
-            mask_image = np.zeros(color_image.shape, dtype=np.uint8)
+            mask_image = np.zeros(image.shape, dtype=np.uint8)
             mask_image[mask[i] == True] = colors[_class[i]]
             mask_list.append(mask_image)
             class_list.append(_class[i])
             score_list.append(round(score[i], 2))
             bbox_list.append([p1, p2])
-    mask_image = mask_list[0]
     # merge masks and color image
-    if (len(mask_list) > 1):
+    if (len(mask_list) > 0):
+        mask_image = mask_list[0]
         for i in range(len(mask_list)):
             mask_image = cv2.bitwise_or(mask_image, mask_list[i])
-    masked_image = cv2.addWeighted(color_image, 0.8, mask_image, 0.2, 0)
+    else:
+        return image
+    masked_image = cv2.addWeighted(image, 0.8, mask_image, 0.2, 0)
     # draw bbox & put class, score text
     for i in range(len(mask_list)):
         p1 = bbox_list[i][0]
@@ -82,65 +87,3 @@ def get_masked_image(color_image, result, threshold=0.5):
                     ' score='+str(score_list[i]), (p1[0], p1[1]-10), 2, 1, colors[class_list[i]], 2)
 
     return masked_image
-
-def save_masked_vid(model, pipeline, classes=None):
-    ws_path = os.getcwd()
-    track_path = os.path.join(ws_path, 'results/track')
-    os.makedirs(track_path, exist_ok=True)
-    index = len(os.listdir(track_path)) + 1
-    save_path = os.path.join(track_path, 'result' + str(index))
-    os.makedirs(save_path, exist_ok=True)
-
-    frame_size = (640, 480)
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    fps = 30.0
-    file_path = os.path.join(save_path, 'result.avi')
-    out = cv2.VideoWriter(file_path, fourcc, fps, frame_size)
-
-    elapsed_time = 0
-    i = 0
-    try:
-        while (True):
-            if (i == 0):
-                i += 1
-                continue
-
-            color_image = get_images(pipeline, 1)
-
-            # get detection result
-            result, pred_time, esc_time = model.get_prediction(color_image, classes)
-
-            if (result == None):
-                print('frame', i, ': prediction time =', round(pred_time, 2), 's', '\tobject not found for the specified classes')
-
-                cv2.imshow('masked image', color_image)
-
-                out.write(color_image)
-                elapsed_time += pred_time
-
-            else:
-                masked_image = get_masked_image(color_image, result)
-
-                print('frame', i, ': prediction time =', round(pred_time, 2), 's\textract specified classes time =', round(esc_time, 2), 's')
-                cv2.imshow('masked image', masked_image)
-
-                # save video
-                out.write(masked_image)
-                elapsed_time += pred_time
-
-            if (cv2.waitKey(1) == 27):
-                break
-
-            i += 1
-
-    finally:
-        elapsed_time = round(elapsed_time, 2)
-        print('-----------------------------------------')
-        print('job finished')
-        print('total elapsed time =', elapsed_time)
-        print('average elapsed time per frame =', round(elapsed_time/i, 2))
-        print('save masked video', file_path)
-        print('-----------------------------------------')
-        pl.stop_pipeline(pipeline)
-        out.release()
-        cv2.destroyAllWindows()
